7244ddc3da178486e1935fb59a9c9605e63ca2b6
BunBunSeo <animsha.inex@gmail.com>
2025-02-13 18:42:31 +0530

    Create draggable.js

---
+++ draggable.js
@@ -0,0 +1,21 @@
+document.querySelectorAll('.love-note').forEach(note => {
+  interact(note).draggable({
+    inertia: true,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ],
+    autoScroll: true,
+    onmove: (event) => {
+      const target = event.target;
+      const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+      const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+      target.style.transform = `translate(${x}px, ${y}px)`;
+      target.setAttribute('data-x', x);
+      target.setAttribute('data-y', y);
+    }
+  });
+});
